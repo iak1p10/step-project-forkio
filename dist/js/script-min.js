572955d1bed1d3d563073951a20f2274f87832b6
Saveliy <saveliy.shaposhnik@gmail.com>
2020-12-06 23:19:17 +0200

    reviews

--- dist/js/script-min.js
+++ dist/js/script-min.js
@@ -1,11 +1,15 @@
 const btn = document.querySelector(".header-list_btn");
 const menu = document.querySelector(".header-list");
 const item = document.getElementsByClassName("header-list_item");
+const btnActive = document.querySelector(".header-list_btn");
 
 btn.addEventListener("click",function(){
     menu.classList.toggle("active");
 });
 
+btnActive.addEventListener("click",function(){
+    btnActive.classList.toggle("active");
+});
 for (let index = 0; index < item.length; index++) {
     item[index].addEventListener("click",function(){
         for (let index = 0; index < item.length; index++) {
