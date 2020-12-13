e57b9cfa11ce36e77bd41e030149dbdce6e241a8
Saveliy <saveliy.shaposhnik@gmail.com>
2020-12-13 19:59:55 +0200

    final

--- gulp-tasks/img.js
+++ gulp-tasks/img.js
@@ -1,11 +1,15 @@
 const {src, dest } = require('gulp');
 const imagemin = require('gulp-imagemin');
  
-const images = () => (
+const images = (cb) => {
     src('./src/img/*')
         .pipe(imagemin())
+        .pipe(dest('./dist/img'));
+    src('./src/img/*/*')
+        .pipe(imagemin())
         .pipe(dest('./dist/img'))
-);
+    cb();
+};
 
 exports.images = images;
 
